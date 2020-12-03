631870323d1037d066beb03871e2ceaa2f308329
Marta Semrau <mm.semrau@gmail.com>
2020-12-03 23:10:02 +0100

    Add populate to employees

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -5,7 +5,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -17,7 +17,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const emp = await Employee.findOne().skip(rand);
+    const emp = await (await Employee.findOne().skip(rand)).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
@@ -31,7 +31,7 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const emp = await Employee.findById(req.params.id);
+    const emp = await (await Employee.findById(req.params.id)).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found' });
     else res.json(emp);
   }
